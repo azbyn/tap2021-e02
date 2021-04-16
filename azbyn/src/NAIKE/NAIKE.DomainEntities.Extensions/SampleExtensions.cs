00b668b9879ff414426e7385048fe107121af79f
azbyn <andreypavel1@yahoo.com>
2021-04-16 08:37:43 +0300

    Added SampleExtensions

---
+++ azbyn/src/NAIKE/NAIKE.DomainEntities.Extensions/SampleExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace NAIKE.DomainEntities.Extensions
+{
+    public static class SampleExtensions
+    {
+        public static int ValidDnaSize = 1000;
+        public static bool IsValidDnaSequence(this Sample sample)
+        {
+            return IsValidDnaSequence(sample.DnaSequence);
+        }
+        public static bool IsValidDnaSequence(string dnaSequence)
+        {
+            if (dnaSequence == null) return false;
+            if (dnaSequence.Length != ValidDnaSize) return false;
+
+            return dnaSequence.Any(c => "ACGT".Contains(char.ToUpper(c)));
+        }
+    }
+}
