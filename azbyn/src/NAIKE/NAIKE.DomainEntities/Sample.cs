c10afa2cfce61eecd42da431aec228c00ff68b91
azbyn <andreypavel1@yahoo.com>
2021-04-16 08:37:00 +0300

    Added Sample class

---
+++ azbyn/src/NAIKE/NAIKE.DomainEntities/Sample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NAIKE.DomainEntities
+{
+    public class Sample
+    {
+        public Guid Id { get; }
+        public string DnaSequence { get; }
+        public DateTime AnalysisTime { get; }
+
+        public Sample(Guid id, string dnaSequence, DateTime analysisTime)
+        {
+            Id = id;
+            DnaSequence = dnaSequence;
+            AnalysisTime = analysisTime;
+        }
+    }
+}
