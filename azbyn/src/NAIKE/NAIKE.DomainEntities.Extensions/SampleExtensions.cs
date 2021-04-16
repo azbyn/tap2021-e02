0fea1517977a2bed6414b1dee6333a7d380a83e7
azbyn <andreypavel1@yahoo.com>
2021-04-16 08:41:04 +0300

    Stylistic changes to function headers

--- azbyn/src/NAIKE/NAIKE.DomainEntities.Extensions/SampleExtensions.cs
+++ azbyn/src/NAIKE/NAIKE.DomainEntities.Extensions/SampleExtensions.cs
@@ -6,11 +6,11 @@ namespace NAIKE.DomainEntities.Extensions
     public static class SampleExtensions
     {
         public static int ValidDnaSize = 1000;
-        public static bool IsValidDnaSequence(this Sample sample)
+        public static bool HasValidDnaSequence(this Sample sample)
         {
             return IsValidDnaSequence(sample.DnaSequence);
         }
-        public static bool IsValidDnaSequence(string dnaSequence)
+        public static bool IsValidDnaSequence(this string dnaSequence)
         {
             if (dnaSequence == null) return false;
             if (dnaSequence.Length != ValidDnaSize) return false;
